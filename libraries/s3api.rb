b41c8c14300678dea6cd0074b6f4d79a022ce592
Peter Halliday <pghalliday@gmail.com>
2015-07-23 16:40:00 +0200

    scope issues

--- libraries/s3api.rb
+++ libraries/s3api.rb
@@ -2,39 +2,40 @@ require 'tempfile'
 
 module S3api
 
-  def aws_authentication (access_key_id, secret_access_key)
-    if access_key_id
-      [
-        "AWS_ACCESS_KEY_ID=#{access_key_id}",
-        "AWS_SECRET_ACCESS_KEY=#{secret_access_key}"
-      ].join(' ')
-    else
-      ''
-    end
-  end
-
-  def aws_config (kms)
-    kms_config = ::File.join(Chef::Config['file_cache_path'], 'kmsconfig')
-    if kms
-      "AWS_CONFIG_FILE=#{kms_config}"
-    else
-      ''
-    end
-  end
-
-  def aws_command (region, bucket, key, path, access_key_id, secret_access_key, kms)
-    [
-      aws_authentication(access_key_id, secret_access_key),
-      aws_config(kms),
-      'aws s3api get-object',
-      "--region #{region}",
-      "--bucket #{bucket}",
-      "--key #{key}",
-      path
-    ].join(' ')
-  end
-
   def S3api.get_object (region, bucket, key, access_key_id, secret_access_key, kms)
+
+    def aws_authentication (access_key_id, secret_access_key)
+      if access_key_id
+        [
+          "AWS_ACCESS_KEY_ID=#{access_key_id}",
+          "AWS_SECRET_ACCESS_KEY=#{secret_access_key}"
+        ].join(' ')
+      else
+        ''
+      end
+    end
+
+    def aws_config (kms)
+      kms_config = ::File.join(Chef::Config['file_cache_path'], 'kmsconfig')
+      if kms
+        "AWS_CONFIG_FILE=#{kms_config}"
+      else
+        ''
+      end
+    end
+
+    def aws_command (region, bucket, key, path, access_key_id, secret_access_key, kms)
+      [
+        aws_authentication(access_key_id, secret_access_key),
+        aws_config(kms),
+        'aws s3api get-object',
+        "--region #{region}",
+        "--bucket #{bucket}",
+        "--key #{key}",
+        path
+      ].join(' ')
+    end
+
     temp_file = Tempfile.new('s3api_json')
     begin
       get_object = Mixlib::ShellOut.new aws_command(region, bucket, key, temp_file.path, access_key_id, secret_access_key, kms)
