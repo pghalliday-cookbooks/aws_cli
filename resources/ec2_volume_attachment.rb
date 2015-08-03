c7c2d333569c888903119ebdd1b2f3ab92b2daf7
Peter Halliday <pghalliday@gmail.com>
2015-08-03 14:47:50 +0200

    forgot file_system attribute

--- resources/ec2_volume_attachment.rb
+++ resources/ec2_volume_attachment.rb
@@ -4,6 +4,7 @@ default_action :attach
 attribute :volume_id, name_attribute: true, kind_of: String, required: true
 attribute :instance_id, kind_of: String, required: true
 attribute :device, kind_of: String, required: true
+attribute :file_system, kind_of: String, required: true
 attribute :mount_point, kind_of: String, required: true
 attribute :access_key_id, kind_of: String
 attribute :secret_access_key, kind_of: String
