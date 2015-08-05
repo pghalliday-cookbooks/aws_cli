edf3c84764c89e4e7d68badcf485fc8af9f99403
Peter Halliday <pghalliday@gmail.com>
2015-08-05 13:38:34 +0200

    ga\h ruby scopinggit add -A

--- libraries/s3api_get_object.rb
+++ libraries/s3api_get_object.rb
@@ -15,31 +15,25 @@ EOH
         temp_config.close
       end
 
-      def aws_authentication (access_key_id, secret_access_key)
-        if access_key_id
-          [
-            "AWS_ACCESS_KEY_ID=#{access_key_id}",
-            "AWS_SECRET_ACCESS_KEY=#{secret_access_key}"
-          ].join(' ')
-        else
-          ''
-        end
-      end
-
-      def aws_command (region, bucket, key, access_key_id, secret_access_key, config)
-        [
-          aws_authentication(access_key_id, secret_access_key),
-          "AWS_CONFIG_FILE=#{config}",
-          'aws s3api get-object',
-          "--region #{region}",
-          "--bucket #{bucket}",
-          "--key #{key}"
+      aws_authentication = ''
+      if access_key_id
+        aws_authentication = [
+          "AWS_ACCESS_KEY_ID=#{access_key_id}",
+          "AWS_SECRET_ACCESS_KEY=#{secret_access_key}"
         ].join(' ')
       end
 
-      command = aws_command region, bucket, key, access_key_id, secret_access_key, temp_config.path
+      aws_command = [
+        aws_authentication,
+        "AWS_CONFIG_FILE=#{temp_config.path}",
+        'aws s3api get-object',
+        "--region #{region}",
+        "--bucket #{bucket}",
+        "--key #{key}"
+      ].join(' ')
+
       begin
-        get_object = Mixlib::ShellOut.new "#{command} #{temp_file.path}"
+        get_object = Mixlib::ShellOut.new "#{aws_command} #{temp_file.path}"
         get_object.run_command
         get_object.error!
         yield File.read(temp_file)
