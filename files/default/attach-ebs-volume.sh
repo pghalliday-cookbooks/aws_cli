cf3d0e9c54873e3dc2f92e5370e5d1fbbcce80b8
Peter Halliday <pghalliday@gmail.com>
2015-08-05 10:48:36 +0200

    debugging

--- files/default/attach-ebs-volume.sh
+++ files/default/attach-ebs-volume.sh
@@ -13,6 +13,8 @@ AWS_SECRET_ACCESS_KEY=$7
 function get_state {
   aws --region $REGION ec2 describe-volumes --volume-ids $VOLUME_ID | $RUBY -e "
   require 'json'
+  json = ARGF.read
+  STDERR.puts json
   description = JSON.parse(ARGF.read)['Volumes'][0]
   state = description['State']
   state = description['Attachments'][0]['State'] if state == 'in-use'
@@ -20,7 +22,7 @@ function get_state {
   "
 }
 
-if ! [ "$(get_state)" == "attached" ]; then
+if [ "$(get_state)" == "available" ]; then
   aws --region $REGION ec2 attach-volume --volume-id $VOLUME_ID --instance-id $INSTANCE_ID --device $DEVICE
 
   while ! [ "$(get_state)" == "attached" ]; do
